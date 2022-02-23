b2fc75c944efea71e40102753c960329b3c0c8b1
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-02-23 20:48:03 +0900

    modified the child to be executed independently from the parent container

--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,10 +1,39 @@
 import faker from 'faker';
 
-let products = '';
+const mount = el => {
+  let products = '';
 
-for (let i = 0; i < 5; i++) {
-  const name = faker.commerce.productName();
-  products += `<div>${name}</div>`;
+  for (let i = 0; i < 5; i++) {
+    const name = faker.commerce.productName();
+    products += `<div>${name}</div>`;
+  }
+
+  el.innerHTML = products;
+};
+
+// Situation #1
+// We are running this file in development in isolation
+// We are using our local index.html file
+// Which Definitely has an element with an id of 'dev-products'
+// We want to immediately render our app into that element
+
+// containerとは別に独立してレンダリングしたいケース
+if (process.env.NODE_ENV === 'development') {
+  const el = document.querySelector('#dev-products');
+  // Assuming our container doesn't have an element
+  // with id 'dev-products' ....
+  if (el) {
+    //   We are probably running in isolation
+    // NOT executing products through our container
+    mount(el);
+  }
 }
 
-document.querySelector('#dev-products').innerHTML = products;
+// Situation #2
+// We are running this file in development or production
+// through the CONTAINER app
+// NO GUARANTEE that an element with an id of 'dev-products'
+// We DO NOT WANT to try immediately render the app
+
+// container経由でレンダリングしたいケース
+export { mount };
